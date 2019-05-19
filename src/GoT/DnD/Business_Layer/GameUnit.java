bd5f4868897ae1f48156eb35056b6838902ad37e
Raz Izak <raziz@post.bgu.ac.il>
2019-05-19 23:50:26 +0300

    GameUnit, Player abstract Classes created

--- src/GoT/DnD/Business_Layer/GameUnit.java
+++ src/GoT/DnD/Business_Layer/GameUnit.java
@@ -2,7 +2,34 @@ package GoT.DnD.Business_Layer;
 
 public abstract class GameUnit {
     public String name;
-    public Integer healthPool;
-    public Integer currHealth;
+    public Integer hp;              //Health Pool
+    public Integer currHP;          //Current Health
+    public Integer ap;              //Attacking Points
+    public Integer dp;              //Defense Points
+    public Integer[] position;
 
+    public GameUnit(String name, Integer hp, Integer currHP, Integer ap, Integer dp, Integer[] position) {
+        this.name = name;
+        this.hp = hp;
+        this.currHP = currHP;
+        this.ap = ap;
+        this.dp = dp;
+        this.position = new Integer[2];
+    }
+
+    public Integer getCurrHP() {
+        return currHP;
+    }
+
+    public void setCurrHP(Integer currHP) {
+        this.currHP = currHP;
+    }
+
+    public Integer[] getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer[] position) {
+        this.position = position;
+    }
 }
