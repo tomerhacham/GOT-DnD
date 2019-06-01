0b706ec1b786fcd24daaacb307149d889b1868bb
Raz Izak <raziz@post.bgu.ac.il>
2019-06-01 20:02:59 +0300

    Enemy toString fix

--- src/GoT/DnD/Business_Layer/Enemy.java
+++ src/GoT/DnD/Business_Layer/Enemy.java
@@ -17,6 +17,7 @@ public abstract class Enemy extends GameUnit {
         return this.getPosition().distance(Hero.getPosition());
     }
 
+    @Override
     public String toString(){
         return getName() + " Health: " + getCurrHP() + " Attack damage: " + getAp() + " Defense: " + getDp();
 
