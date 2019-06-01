c96b4bbdb214dd69e768d4b284ce18767aa27afb
Raz Izak <raziz@post.bgu.ac.il>
2019-06-01 18:49:40 +0300

    Controller choosePlayer func added

--- src/GoT/DnD/Controller.java
+++ src/GoT/DnD/Controller.java
@@ -15,6 +15,19 @@ public class Controller {
 
  */
 
+    public static int choosePlayer(){
+        Scanner reader = new Scanner(System.in);
+        String validInput = "123456";
+        int input;
+        do {
+            while (!reader.hasNextInt()) {
+                reader.next();
+            }
+            input = reader.nextInt();
+        } while(input < 1 || input > 6);
+        return input;
+    }
+
     public static String getInput(){
         Scanner reader = new Scanner(System.in);
         String validInput = "wsadeq";
